1d0e6050e4a92591810b5961ba3516297258f99f
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-10 23:47:17 +0100

    Incremental Update

---
+++ src/test/java/dev/gxsoft/blogrestjpa/BlogRestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gxsoft.blogrestjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
